3a10bffd3f6dfe1e745f0837c654d236bf834721
Thayna Cristina <thayrcristina@gmail.com>
2020-01-22 15:30:16 -0300

    Criação dos componentes

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from './styles'; 
+export default function Header(){
+    return (
+        <Container>
+            <Top>
+                <Logo source={} />
+                <Title>Thayna</Title>
+            </Top>
+
+        </ Container>
+
+    );
+}
