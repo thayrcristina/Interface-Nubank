dd00e07e377adca755c33eaf9fb04a588402e36d
Thayna Cristina <thayrcristina@gmail.com>
2020-01-23 14:31:05 -0300

    Craição dos componentes visuais

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Container, TabText, TabsContainer, TabItem } from './styles'; 
+import { Container, TabText, TabsContainer, TabItem } from './styles';
 
 
-export default function Header(){
+export default function Header() {
     return (
         <Container>
-         <TabsContainer>
-             <TabItem>
-                <Icon name="person-add" size={24} color="#FFF" />
-                <TabText>Indicar Amigos</TabText>
-             </TabItem>             
-        </TabsContainer>                
-         </Container>
+            <TabsContainer>
+                <TabItem>
+                    <Icon name="person-add" size={24} color="#FFF" />
+                    <TabText>Indicar Amigos</TabText>
+                </TabItem>
+                <TabItem>
+                    <Icon name="chat-bubble-outline" size={24} color="#FFF" />
+                    <TabText>Cobrar</TabText>
+                </TabItem>
+                <TabItem>
+                    <Icon name="arrow-downward" size={24} color="#FFF" />
+                    <TabText>Depositar</TabText>
+                </TabItem>
+                <TabItem>
+                    <Icon name="arrow-upward" size={24} color="#FFF" />
+                    <TabText>Transferir</TabText>
+                </TabItem>
+                <TabItem>
+                    <Icon name="lock" size={24} color="#FFF" />
+                    <TabText>Bloquear cartão</TabText>
+                </TabItem>
+            </TabsContainer>
+        </Container>
     );
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
-import { Container } from './styles';
+import { Container, Content, Card, CardHeader, CardFooter,
+   CardContent, Title, Description, Annotation} from './styles';
 
 
 export default function Main() {
@@ -11,9 +14,23 @@ export default function Main() {
   return (
     <Container>      
       <Header />
+      <Content>
+        <Menu />
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$250.458,78</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>Transferência de R$20,00 recebida de Marcos Oliveiras hoje às 06:00h</Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
       <Tabs />
     </ Container>
-
   );
-
 }
